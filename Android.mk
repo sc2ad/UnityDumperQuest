1e5f3923a59c63abce10a8783424cae19876ad14
Adam ? <adamznow@gmail.com>
2020-06-28 02:53:38 -0700

    Update to latest bs-hook, update Android.mk

--- Android.mk
+++ Android.mk
@@ -31,21 +31,23 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 # Build the beatsaber-hook shared library, SPECIFICALLY VERSIONED!
 include $(CLEAR_VARS)
-LOCAL_MODULE	        := beatsaber-hook_2019_2_1f1_0_1_1
-LOCAL_SRC_FILES         := ./include/libs/libbeatsaber-hook_2019_2_1f1_0_1_1.so
+LOCAL_MODULE	        := beatsaber-hook_2019_2_1f1_0_2_0
+LOCAL_SRC_FILES         := ./include/libs/libbeatsaber-hook_2019_2_1f1_0_2_0.so
 LOCAL_EXPORT_C_INCLUDES := ./extern/beatsaber-hook/shared/
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 # Include the two libraries
 LOCAL_SHARED_LIBRARIES := modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_2019_2_1f1_0_1_1
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_2019_2_1f1_0_2_0
 LOCAL_LDLIBS     := -llog
 LOCAL_CFLAGS     := -I"c:/Program Files/Unity/Editor/Data/il2cpp/libil2cpp"
 LOCAL_MODULE     := dumper
 LOCAL_CPPFLAGS   := -std=c++2a
 LOCAL_C_INCLUDES := ./include ./src
 LOCAL_SRC_FILES  += $(call rwildcard,src/,*.cpp)
+LOCAL_SRC_FILES  += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
+LOCAL_SRC_FILES  += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 include $(BUILD_SHARED_LIBRARY)
 
 # In order to make this mod work with BMBF, you must provide a zip file with the specific libbeatsaber-hook.so (file copied to the libs directory)
