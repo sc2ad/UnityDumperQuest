680cfe869c0bedf790078fd9de956163350833ab
Lauriethefish <lauriethefish@outlook.com>
2021-03-19 18:56:07 +0000

    BS-hook 1.2.3

--- Android.mk
+++ Android.mk
@@ -26,11 +26,11 @@ LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 1.2.2
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.2.3
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_2_2
+LOCAL_MODULE := beatsaber-hook_1_2_3
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_2_2.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_2_3.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -39,7 +39,7 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_2_2
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_2_3
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -I"extern/" -DID='"UnityDumperQuest"' -DVERSION='"1.0.0"' -I'./shared' -I'./extern'
 LOCAL_CPPFLAGS += -std=c++2a
--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -7,5 +7,5 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libdumper.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_2.so", "./bmbfmod.json" -DestinationPath "./dumper_v0.1.0.zip" -Force
+Compress-Archive -Path "./libs/arm64-v8a/libdumper.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_3.so", "./bmbfmod.json" -DestinationPath "./dumper_v0.1.0.zip" -Force
 Write-Output "Success!"
